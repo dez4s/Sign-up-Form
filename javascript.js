12a8ce752fcc3517a1477ad7a7022fcb8833dea8
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-02-16 02:28:20 +0200

    Implement form validation through CSS

--- javascript.js
+++ javascript.js
@@ -1,38 +1,6 @@
-const inputs = document.querySelectorAll('input:not([id="pass-confirm"])');
-
-console.log(inputs);
-
-inputs.forEach(input => {
-    const paraInput = input.nextElementSibling;
-
-    input.addEventListener('blur', (e) => {
-        if (!e.target.validity.valid && e.target.value) {
-            paraInput.textContent = e.target.title;
-            if (!e.target.title) {
-                paraInput.textContent = e.target.validationMessage;
-            }
-        }
-    });
-
-    input.addEventListener('input', (e) => {
-        if (e.target.validity.valid) {
-            paraInput.textContent = "";
-        }
-    })
-});
-
 const passConfirm = document.getElementById('pass-confirm');
 const pass = document.getElementById('pass');
 const paraConfirm = passConfirm.parentElement.lastElementChild;
-
-// passConfirm.addEventListener('blur', (e) => {
-
-//     if ((passConfirm.value !== pass.value) && pass.value) {
-//         passConfirm.setCustomValidity('Passwords do not match');
-//         paraConfirm.textContent = passConfirm.validationMessage;
-//         passConfirm.style.border = "1px solid red";
-//     } 
-// });
 
 passConfirm.addEventListener('input', () => {
     
